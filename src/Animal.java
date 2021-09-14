2ceb4df0ffded9e4eaa79afc67382b1341e663a4
phamtanphat <phatdroid94@gmail.com>
2021-09-14 21:03:01 +0700

    Initial commit

---
+++ src/Animal.java
@@ -0,0 +1,6 @@
+public class Animal {
+    // tính chất - thuộc tính (properties)
+    int height;
+    String color;
+    int weight;
+}
---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args){
+
+        // class vs object
+//        class -> object
+
+        // Khởi tạo object
+
+        // kieudulieu tenbien
+        // ctrl + space : gợi ý code
+        Animal cat = new Animal();
+        cat.height = 1;
+        cat.color = "gray";
+        cat.weight = 3;
+        System.out.println(cat);
+    }
+}
