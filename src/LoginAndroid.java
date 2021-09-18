b83be7a97f6ebbee0902565d4adcf6acc62162d1
phamtanphat <phatdroid94@gmail.com>
2021-09-18 21:20:16 +0700

    interface

---
+++ src/LoginAndroid.java
@@ -0,0 +1,13 @@
+public class LoginAndroid implements OnLogin{
+    @Override
+    public void onValidate() {
+
+    }
+
+    @Override
+    public void onSubmit() {
+
+    }
+}
+//extend (kế thừa) : sở hữu
+//implement (kế thừa) : phải xây dựng
---
+++ src/OnLogin.java
@@ -0,0 +1,4 @@
+public interface OnLogin {
+    void onValidate();
+    void onSubmit();
+}
